11493f7c989de652ee7659c4e068aeb6775ebba2
Ajani Bilby <aj@bilby.net>
2020-03-18 18:09:38 +1100

    ~Fixed large file transfers breaking
    Large file transfers broke using previous method due to it being broken into multiple packets

--- decoders/multipart-form.js
+++ decoders/multipart-form.js
@@ -1,101 +1,84 @@
 const EventEmitter = require('events');
-var fs = require('fs');
 
 module.exports = function(stream, boundry){
   var listener = new EventEmitter();
 
-  var id = boundry;
+  boundry = "--"+boundry;
   var first = true;
-  var ended = false;
   var forms = {};
-	var lastName;
-
-  var dispositionNext = false;
-  var spacerNext = false;
+  var lastName;
 
   stream.on('data', function(data){
-    if (ended){
-      return;
-    }
+    while (data.length > 0){
+      index = data.indexOf(boundry);
 
-    var index;
-    if (first){
-      index = data.indexOf('\n');
-      id = data.slice(0, index-1).toString();
-    }
-
-    index = data.indexOf('\r\n');
-    while (index != -1){
-      var content = data.slice(0, index+2);
-      data = data.slice(index+2);
-
-      if (content.indexOf(id) === 0){
-        if (content.slice(id.length).indexOf('--') === 0){
-          listener.emit('finish');
-          ended = true;
-          break;
-        }else{
-          dispositionNext = true;
-        }
-      }else{
-        if (dispositionNext && content.indexOf('Content-Disposition:') === 0){
-          content = content.slice(21);
-
-          if (content.indexOf('form-data; ') === 0){
-            content = content.slice(11, -2);
-
-            var info = {};
-            content = content.toString().split('; ');
-            for (let item of content){
-              item = item.split('=');
-              info[item[0]] = item[1].slice(1, -1);
-            }
-
-            if (lastName){
-              listener.emit('end', lastName, new Object(forms[lastName]));
-            }
-
-            if (info.name){
-              forms[info.name] = info;
-              lastName = info.name;
-            }
-
-            dispositionNext = false;
-            spacerNext = true;
-          }
-        }else if (spacerNext){
-          if (content.indexOf('Content-Type: ') === 0){
-            content = content.slice(14);
-            forms[lastName]['Content-Type'] = content;
-          }else{
-            spacerNext = false;
-          }
-        }else{
-          if (lastName){
-            if (content.indexOf('\r\n') === content.length-2){
-              if (data.indexOf(id) === 0){
-                content = content.slice(0, content.length-2);
-              }
-            }
-
-            listener.emit('data', lastName, new Object(forms[lastName]), content);
-          }
-        }
+      // All of this data is just the previous feild continuing
+      if (index == -1) {
+        console.log(22, -1);
+        listener.emit('data', lastName, forms[lastName], data);
+        return;
+      }
+    
+      // Push the chunk of any continuing data
+      if (index > 0) {
+        listener.emit('data', lastName, forms[lastName], data.slice(0, index-2)); // -2 to remove \r\n
+        data = data.slice(index);
+        continue;
       }
 
-      index = data.indexOf('\r\n');
-    }
+      // Remove boundry tag
+      data = data.slice(index+boundry.length);
 
-    first = false;
+      // End of form
+      if (data.indexOf('--') == 0) {
+        return;
+      } else {
+        // Push message that previous field ended
+        if (!first) {
+          listener.emit('end', lastName, forms[lastName]);
+        } else {
+          first = false;
+        }
+
+        // Strip off metadata
+        let index = data.indexOf('Content-Disposition: form-data; ') + 32;
+        data = data.slice(index);
+        index = data.indexOf('\r\n\r\n');
+        let meta = data.slice(0, index).toString().split('\r\n');
+        data = data.slice(index+4);
+
+        // Read form meta-data
+        let info = {};
+        meta[0] = meta[0].split('; ');
+        for (let item of meta[0]){
+          item = item.split('=');
+          info[item[0]] = item[1].slice(1, -1);
+        }
+
+        // Read any extra form meta-data
+        for (let i=1; i<meta.length; i++) {
+          let item = meta[i].split(': ');
+          console.log(item);
+          info[ item[0] ] = item[1];
+          console.log(info);
+        }
+
+        // Store field metadata
+        if (info.name){
+          forms[info.name] = info;
+          lastName = info.name;
+        }
+      }
+    }
   });
 
   //Incase the post is missing the end for some reason
   stream.on('end', function(){
-    if (!ended){
-      ended = true;
-      listener.emit('end', lastName, new Object(forms[lastName]));
-      listener.emit('finish');
-    }
+    // Push message that previous field ended
+    listener.emit('end', lastName, forms[lastName]);
+
+    // Push message that entire form has been submited
+    listener.emit('finish');
   });
 
   return listener;
